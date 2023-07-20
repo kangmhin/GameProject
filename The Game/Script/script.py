60320e2a8191d86af5b2d9d67a8f9284397668e2
khybin <134588918+khybin@users.noreply.github.com>
2023-07-20 17:49:38 +0900

    Update script.py

--- The Game/Script/script.py
+++ The Game/Script/script.py
@@ -1,46 +1,25 @@
-import sys
 import pygame
+import sys
 
-pygame.init()
+#화면 크기
+screen_height = 750
+screen_width = 1280
 
-# 게임 시작화면
-def game_start() :
-    start_screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('The Game')
 
-    pygame.font.init()
-    start_font = pygame.font.SysFont('Sans', 40, True, True)
-    start_message = 'Start'
-    start_message_object = start_font.render(start_message, True, (0, 0, 0))
-    start_message_rect = start_message_object.get_rect()
-    start_message_rect.center = (250, 250)
 
-    while True :
-        for event in pygame.event.get() :
-            if event.type == pygame.QUIT :
-                pygame.quit()
-                sys.exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                return
-            start_screen.fill((255, 255, 255))
-            start_screen.blit(start_message_object, start_message_rect)
-            pygame.display.update()
 
-game_start()
+#게임 실행
+run = True
+while run :
 
-pygame.init()
-
-fps = pygame.time.Clock()
-
-background = pygame.display.set_mode((500, 500))
-pygame.display.set_caption("SONOL")
-
-background.fill((255, 255, 255))
-
-play = True
-while play:
-    deltaTime = fps.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            play = False
+    #event 값
+    for event in pygame.event.get() :
+        #게임 종료
+        if event.type == pygame.QUIT :
+            run = False
+    
+    pygame.display.update()
 
 pygame.quit()
